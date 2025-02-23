a4f2a748c8b69870d48e749b4e6878d717a91d20
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-23 20:39:02 +0530

    created login page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index'); // Added title to render context
+  res.render('index'); 
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register'); 
 });
 
 module.exports = router;
