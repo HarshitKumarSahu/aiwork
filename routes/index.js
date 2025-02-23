80229135c066067fdbfd9b62644dda17a051af57
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-02-23 21:40:50 +0530

    added passport authenticate for /register

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const userModel =  require("./users");
+const passport = require('passport');
 
 router.get('/', function(req, res, next) {
   res.render('index'); 
@@ -11,6 +13,25 @@ router.get('/login', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('register'); 
+});
+
+router.post('/register', function(req, res, next) {
+  const data = new userModel({
+    username : req.body.username,
+    email : req.body.email,
+    contact : req.body.contact,
+  })
+
+  userModel.register(data, req.body.password)
+    .then(function() {
+      passport.authenticate("local")(req,res, function() {
+        res.render("/profile")
+      })
+    })
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile'); 
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/aiwork"; // Use environment variable or default
+const mongoURI ="mongodb://127.0.0.1:27017/aiwork"; 
 
-module.exports = router;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+
+const userSchema = mongoose.Schema({
+    username : String,
+    name : String,
+    email : String,
+    password : String,
+    profileImage : String,
+    contact : Number,
+    boards : {
+      type : Array,
+      default: []
+    }
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
