67b181045a34d989edbfea74affb2be8469a24c7
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-05-21 21:30:50 +0530

    final push 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
 
 // Session and Passport requires
 const expressSession = require("express-session");
@@ -27,7 +27,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Middlewares
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
